ab4028c533c03f18fc5f2c2f41b64bcce3f54b6d
= <=>
2022-03-28 13:40:36 -0600

    initial javadocs

---
+++ docs/javadoc/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"Acquire","c":"App","l":"App()","u":"%3Cinit%3E()"},{"p":"Acquire","c":"App","l":"getGreeting()"},{"p":"Acquire","c":"App","l":"main(String[])","u":"main(java.lang.String[])"}];updateSearchResults();
