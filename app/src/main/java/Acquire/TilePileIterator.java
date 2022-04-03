784468667b58bed73df2eec43e5583dbf5774a74
Ben Thomas <thomben4@isu.edu>
2022-04-02 18:40:59 -0600

    Wrote java docs for factories and related classes

--- app/src/main/java/Acquire/TilePileIterator.java
+++ app/src/main/java/Acquire/TilePileIterator.java
@@ -1,27 +1,46 @@
 package Acquire;
 
-import java.util.Collection;
-import java.util.EmptyStackException;
-import java.util.Iterator;
-import java.util.LinkedList;
+import java.util.*;
 
+/**
+ * An implementation of an iterator for iterating through a collection of tiles. This class is meant to be used
+ * within a {@link TilePile} implementation.
+ * @author Ben Thomas
+ */
 public class TilePileIterator implements Iterator<Tile> {
     private final LinkedList<Tile> tiles;
+    private final LinkedList<Tile> tilePile;
+    private Tile current = null;
 
+    /**
+     * Constructs a new TilePileIterator object with a collection of tiles
+     * @param tiles which the iterator will iterate through
+     */
     public TilePileIterator(Collection<Tile> tiles) {
         this.tiles = new LinkedList<>(tiles);
+        this.tilePile = (LinkedList<Tile>) tiles;
     }
 
+    /** {@inheritDoc} */
     @Override
     public boolean hasNext() {
         return !tiles.isEmpty();
     }
 
+    /** {@inheritDoc} */
     @Override
     public Tile next() {
         if (tiles.isEmpty()) {
-            throw new EmptyStackException();
+            throw new NoSuchElementException();
         }
-        return tiles.removeFirst();
+        current = tiles.removeFirst();
+        return current;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public void remove() {
+        if (current == null) throw new IllegalStateException();
+        tilePile.remove(current);
     }
 }
