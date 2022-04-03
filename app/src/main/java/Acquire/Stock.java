4fc39cbd29d6435b5beb2120ead5d301bd53b345
Ben Thomas <thomben4@isu.edu>
2022-04-02 18:40:59 -0600

    Wrote java docs for factories and related classes

--- app/src/main/java/Acquire/Stock.java
+++ app/src/main/java/Acquire/Stock.java
@@ -3,11 +3,19 @@ package Acquire;
 import lombok.Getter;
 import lombok.Setter;
 
+/**
+ * Holds stock information
+ * @author Ben Thomas
+ */
 public class Stock {
     @Getter private final Corporation corp;
-    @Getter @Setter private int price;
+    @Getter @Setter private int price = 0;
 
-    public Stock(Corporation corp) {
-        this.corp = corp;
+    /**
+     * Constructs a Stock with the given {@link Corporation} as its corporation
+     * @param corporation the stock is a part of
+     */
+    public Stock(Corporation corporation) {
+        this.corp = corporation;
     }
 }
