e8f0e942fbf8c585828e447d602fd78079ecb69d
Grant Madson <grantmadson@gmail.com>
2022-04-10 05:04:08 -0600

    A commit to allow remote to have this branch again.

--- app/src/main/java/Acquire/MainGame.java
+++ app/src/main/java/Acquire/MainGame.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 
 public class MainGame extends Application {
+    
     @Override
     public void start(Stage primaryStage) throws Exception {
 
