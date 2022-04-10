a8e726ed3e3ab2785da97c05b9c35bb3b7df2964
Savannah Webb <webbsav2@isu.edu>
2022-04-10 15:58:09 -0600

    NewOrLoadGame.fxml for when Start Game is pressed.

---
+++ app/src/main/java/Acquire/NewOrLoadGameClass.java
@@ -0,0 +1,8 @@
+package Acquire;
+
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+public class NewOrLoadGameClass extends Scene {
+    public NewOrLoadGameClass(Parent root) {super(root);}
+}
