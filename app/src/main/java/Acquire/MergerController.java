e0dfcb0be715fa693eafca868e5165e95865fe5e
Ben Thomas <thomben4@isu.edu>
2022-04-22 19:19:45 -0600

    merger options fxml and controller

---
+++ app/src/main/java/Acquire/MergerController.java
@@ -0,0 +1,28 @@
+package Acquire;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class MergerController {
+
+    @FXML
+    void buyStock(ActionEvent event) {
+
+    }
+
+    @FXML
+    void endTurn(ActionEvent event) {
+
+    }
+
+    @FXML
+    void sellStock(ActionEvent event) {
+
+    }
+
+    @FXML
+    void tradeStock(ActionEvent event) {
+
+    }
+
+}
