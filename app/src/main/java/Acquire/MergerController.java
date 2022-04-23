1553e37c69798b6f9d58002dd36f9ab7fa0a49cd
Ben Thomas <thomben4@isu.edu>
2022-04-22 21:09:31 -0600

    players can no longer buy on a merge as per game rules

--- app/src/main/java/Acquire/MergerController.java
+++ app/src/main/java/Acquire/MergerController.java
@@ -12,18 +12,6 @@ import java.io.IOException;
 public class MergerController {
 
     @FXML
-    void buyStock(ActionEvent event) throws IOException {
-        Stage stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
-        FXMLLoader loader = new FXMLLoader(App.class.getResource("BuyStock.FXML"));
-
-        Scene scene = new Scene(loader.load());
-        stage.setResizable(true);
-        stage.setTitle("Buy Stock");
-        stage.setScene(scene);
-        stage.show();
-    }
-
-    @FXML
     void endTurn(ActionEvent event) {
         Stage stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
         stage.close();
