0de37e7267558d87120c51aba33e95c43b8754e6
Ben Thomas <thomben4@isu.edu>
2022-04-22 21:07:14 -0600

    merger opens buy stock

--- app/src/main/java/Acquire/MergerController.java
+++ app/src/main/java/Acquire/MergerController.java
@@ -12,8 +12,15 @@ import java.io.IOException;
 public class MergerController {
 
     @FXML
-    void buyStock(ActionEvent event) {
+    void buyStock(ActionEvent event) throws IOException {
+        Stage stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        FXMLLoader loader = new FXMLLoader(App.class.getResource("BuyStock.FXML"));
 
+        Scene scene = new Scene(loader.load());
+        stage.setResizable(true);
+        stage.setTitle("Buy Stock");
+        stage.setScene(scene);
+        stage.show();
     }
 
     @FXML
