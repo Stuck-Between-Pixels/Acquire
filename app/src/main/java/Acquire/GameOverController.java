38e0b981e670a0b7ee23581505d8e9e5b621443c
Ben Thomas <thomben4@isu.edu>
2022-04-23 20:19:24 -0600

    added stuff to GameOverController

--- app/src/main/java/Acquire/GameOverController.java
+++ app/src/main/java/Acquire/GameOverController.java
@@ -1,4 +1,23 @@
 package Acquire;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
 public class GameOverController {
+    @FXML private Label playerFourMoney;
+    @FXML private Label playerFourTag;
+    @FXML private Label playerOneMoney;
+    @FXML private Label playerOneTag;
+    @FXML private Label playerThreeMoney;
+    @FXML private Label playerThreeTag;
+    @FXML private Label playerTwoMoney;
+    @FXML private Label playerTwoTag;
+    @FXML private Label winnerTag;
+
+    @FXML
+    void exitGameClicked(ActionEvent event) {
+
+    }
+
 }
